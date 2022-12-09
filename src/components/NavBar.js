63fb9db1aaf3fa72e18df3eaefbffcd2fb59d5f5
kethnal <kaseyvaldez@hotmail.com>
2022-12-08 23:54:30 -0800

    Initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import "./NavBar.scss";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <div className="name">
+        <h1>kasey valdez</h1>
+      </div>
+      <div className="navigations">
+        <h1>about</h1>
+        <h1>projects</h1>
+        <h1>contact</h1>
+        <h1>resume</h1>
+      </div>
+    </nav>
+  )
+}
