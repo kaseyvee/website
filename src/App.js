b5779cd39e9962935f6045415a4909ac3bda40e8
kethnal <kaseyvaldez@hotmail.com>
2022-12-09 02:32:44 -0800

    Set up page views

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.scss';
 import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <>
       <NavBar />
-      <div>
+      <div className='App'>
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
       </div>
     </>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,45 @@
-import "./NavBar.scss";
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
 
 export default function NavBar() {
   return (
     <nav>
-      <div className="name">
-        <h1>kasey valdez</h1>
-      </div>
-      <div className="navigations">
-        <h1>about</h1>
-        <h1>projects</h1>
-        <h1>contact</h1>
-        <h1>resume</h1>
+      <div>
+        <div>
+          <Tooltip title="home">
+            <IconButton>
+              <a href="#home"><i class="fa-solid fa-house"></i></a>
+            </IconButton>
+          </Tooltip>
+        </div>
+        <div>
+          <Tooltip title="about">
+            <IconButton>
+              <a href="#about"><i class="fa-solid fa-user"></i></a>
+            </IconButton>
+          </Tooltip>
+        </div>
+        <div>
+          <Tooltip title="projects">
+            <IconButton>
+              <a href="#projects"><i class="fa-solid fa-folder-open"></i></a>
+            </IconButton>
+          </Tooltip>
+        </div>
+        <div>
+          <Tooltip title="contact me">
+            <IconButton>
+              <a href="#contact"><i class="fa-solid fa-envelope"></i></a>
+            </IconButton>
+          </Tooltip>
+        </div>
+        <div>
+          <Tooltip title="resume">
+            <IconButton>
+              <a href='https://resume.creddle.io/resume/hje8hgzfv4a' target="_blank" rel="noreferrer noopener"><i class="fa-solid fa-file"></i></a>
+            </IconButton>
+          </Tooltip>
+        </div>
       </div>
     </nav>
   )
