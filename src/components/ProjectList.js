0fc949c27da288ff6151e0ce3621d95827e5fb1a
kethnal <kaseyvaldez@hotmail.com>
2022-12-11 10:33:01 -0800

    Code clean up

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,37 +1,38 @@
 import StackList from "./StackList"
 
-const projects = [
-  {
-    name: "teebo",
-    description: "A discourse app that caters to passionate tv fans who want to have active discussions about their favourite shows.",
-    image: "https://i.imgur.com/kZwhgMa.png",
-    techStack: ["Node.js", "Express", "Sass", "React", "PostgreSQL"],
-    link: "https://github.com/muhammad-usama12/teebo"
-  },
-  {
-    name: "pikmeup",
-    description: "A food order pick-up application that allows customers to receive SMS updates regarding the status of their order.",
-    image: "https://i.imgur.com/XisKtLq.png",
-    techStack: ["Node.js", "Express", "Sass", "jQuery", "PostgreSQL"],
-    link: "https://github.com/nicohsfu/pikmiup"
-  },
-  {
-    name: "Interview Scheduler",
-    description: "A single page application that allows users to book interviews with available interviewers.",
-    image: "https://i.imgur.com/DEi4qx5.png",
-    techStack: ["Node.js", "React", "Storybook", "Jest", "Cypress"],
-    link: "https://github.com/kaseyvee/scheduler"
-  },
-  {
-    name: "Jungle",
-    description: "A mini plant-centric e-commerce application that allows users to pay through Stripe.",
-    image: "https://i.imgur.com/lvBpWha.png",
-    techStack: ["Ruby", "Rails", "Cypress", "RSpec", "Stripe"],
-    link: "https://github.com/kaseyvee/jungle-rails"
-  }
-]
-
 export default function ProjectList() {
+  
+  const projects = [
+    {
+      name: "teebo",
+      description: "A discourse app that caters to passionate tv fans who want to have active discussions about their favourite shows.",
+      image: "https://i.imgur.com/kZwhgMa.png",
+      techStack: ["Node.js", "Express", "Sass", "React", "PostgreSQL"],
+      link: "https://github.com/muhammad-usama12/teebo"
+    },
+    {
+      name: "pikmeup",
+      description: "A food order pick-up application that allows customers to receive SMS updates regarding the status of their order.",
+      image: "https://i.imgur.com/XisKtLq.png",
+      techStack: ["Node.js", "Express", "Sass", "jQuery", "PostgreSQL"],
+      link: "https://github.com/nicohsfu/pikmiup"
+    },
+    {
+      name: "Interview Scheduler",
+      description: "A single page application that allows users to book interviews with available interviewers.",
+      image: "https://i.imgur.com/DEi4qx5.png",
+      techStack: ["Node.js", "React", "Storybook", "Jest", "Cypress"],
+      link: "https://github.com/kaseyvee/scheduler"
+    },
+    {
+      name: "Jungle",
+      description: "A mini plant-centric e-commerce application that allows users to pay through Stripe.",
+      image: "https://i.imgur.com/lvBpWha.png",
+      techStack: ["Ruby", "Rails", "Cypress", "RSpec", "Stripe"],
+      link: "https://github.com/kaseyvee/jungle-rails"
+    }
+  ];
+
   const projectList = projects.map(project => {
     return (
       <div>
--- src/components/SkillsList.js
+++ src/components/SkillsList.js
@@ -1,45 +1,46 @@
 import Tooltip from '@mui/material/Tooltip';
 
-const skills = [
-  {
-    name: "JavaScript",
-    iconClass: "fa-brands fa-square-js"
-  },
-  {
-    name: "Node.js",
-    iconClass: "fa-brands fa-node"
-  },
-  {
-    name: "HTML5",
-    iconClass: "fa-brands fa-html5"
-  },
-  {
-    name: "Git",
-    iconClass: "fa-brands fa-git-alt"
-  },
-  {
-    name: "React",
-    iconClass: "fa-brands fa-react"
-  },
-  {
-    name: "Sass",
-    iconClass: "fa-brands fa-sass"
-  },
-  {
-    name: "Figma",
-    iconClass: "fa-brands fa-figma"
-  }
-];
-
-const skillsList = skills.map(skill => {
-  return (
-    <Tooltip title={skill.name}>
-      <i className={skill.iconClass}></i>
-    </Tooltip>
-  )
-})
-
 export default function SkillsList() {
+
+  const skills = [
+    {
+      name: "JavaScript",
+      iconClass: "fa-brands fa-square-js"
+    },
+    {
+      name: "Node.js",
+      iconClass: "fa-brands fa-node"
+    },
+    {
+      name: "HTML5",
+      iconClass: "fa-brands fa-html5"
+    },
+    {
+      name: "Git",
+      iconClass: "fa-brands fa-git-alt"
+    },
+    {
+      name: "React",
+      iconClass: "fa-brands fa-react"
+    },
+    {
+      name: "Sass",
+      iconClass: "fa-brands fa-sass"
+    },
+    {
+      name: "Figma",
+      iconClass: "fa-brands fa-figma"
+    }
+  ];
+  
+  const skillsList = skills.map(skill => {
+    return (
+      <Tooltip title={skill.name}>
+        <i className={skill.iconClass}></i>
+      </Tooltip>
+    )
+  })
+
   return (
     <div className="skills">
       {skillsList}
