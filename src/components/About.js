be328865b029f956a7cb2cb7accbb2883ee8b058
kethnal <kaseyvaldez@hotmail.com>
2022-12-11 09:13:01 -0800

    Style projects page

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,4 @@
+import { Tooltip } from "@mui/material"
 import SkillsList from "./SkillsList"
 
 export default function About() {
@@ -6,11 +7,13 @@ export default function About() {
       <div className='about-container'>
         <h1>About me</h1>
         <div className="bio">
-          <img
-            className="picture"
-            src="https://avatars.githubusercontent.com/u/111031789?v=4"
-            alt="profile"
-          ></img>
+          <Tooltip title="me looking swag in Dublin" placement="left">
+            <img
+              className="picture"
+              src="https://avatars.githubusercontent.com/u/111031789?v=4"
+              alt="profile"
+            ></img>
+          </Tooltip>
           <div className="intro">
             Hello! I'm based in Vancouver, BC, transitioning from a background in chemistry.<br/><br/> My love for web development leans towards the front-end and executing beautiful, clean and minimalist deisigns. I'm keen to provide users an easy and intuitive way to navigate information without being overwhelmed (that's me- I'm users).
           </div>
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -5,25 +5,29 @@ const projects = [
     name: "teebo",
     description: "A discourse app that caters to passionate tv fans who want to have active discussions about their favourite shows.",
     image: "https://i.imgur.com/kZwhgMa.png",
-    techStack: ["Node.js", "Express", "Sass", "React", "PostgreSQL", "Axios"]
+    techStack: ["Node.js", "Express", "Sass", "React", "PostgreSQL"],
+    link: "https://github.com/muhammad-usama12/teebo"
   },
   {
     name: "pikmeup",
     description: "A food order pick-up application that allows customers to receive SMS updates regarding the status of their order.",
     image: "https://i.imgur.com/XisKtLq.png",
-    techStack: ["Node.js", "Express", "Sass", "jQuery", "PostgreSQL"]
+    techStack: ["Node.js", "Express", "Sass", "jQuery", "PostgreSQL"],
+    link: "https://github.com/nicohsfu/pikmiup"
   },
   {
     name: "Interview Scheduler",
     description: "A single page application that allows users to book interviews with available interviewers.",
     image: "https://i.imgur.com/DEi4qx5.png",
-    techStack: ["Node.js", "React", "Webpack", "Axios", "Storybook", "Jest", "Cypress"]
+    techStack: ["Node.js", "React", "Storybook", "Jest", "Cypress"],
+    link: "https://github.com/kaseyvee/scheduler"
   },
   {
     name: "Jungle",
     description: "A mini plant-centric e-commerce application that allows users to pay through Stripe.",
     image: "https://i.imgur.com/lvBpWha.png",
-    techStack: ["Node.js", "Express", "Sass", "React", "PostgreSQL", "Axios", "RSpec"]
+    techStack: ["Ruby", "Rails", "Cypress", "RSpec", "Stripe"],
+    link: "https://github.com/kaseyvee/jungle-rails"
   }
 ]
 
@@ -32,11 +36,11 @@ export default function ProjectList() {
     return (
       <div>
         <div className="project-description">
-          <h2>{project.name}</h2>
+          <a href={project.link}><h2>{project.name}</h2></a>
           <p>{project.description}</p>
+          <StackList stack={project.techStack}/>
         </div>
-        <img src={project.image} alt={project.name}/>
-        <StackList stack={project.techStack}/>
+        <a href={project.link} target="_blank" rel="noreferrer noopener"><img src={project.image} alt={project.name}/></a>
       </div>
     )
   })
