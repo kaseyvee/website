0655a5d8667aa25e1488020d12e08d93a1589db6
kethnal <kaseyvaldez@hotmail.com>
2022-12-11 05:51:26 -0800

    Update contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,23 @@
+import { Tooltip } from "@mui/material";
+
 export default function Contact() {
   return (
     <div id="contact" className="page">
-      <i className="fa-brands fa-github"></i>
+      <div>
+        <h1>Where to find me</h1>
+        <p>Even if you just wanna say hi, my inbox is always open. :)</p>
+      </div>
+      <div className="links">
+      <Tooltip title="GitHub">
+        <a href="https://github.com/kaseyvee"><i className="fa-brands fa-github"></i></a>
+      </Tooltip>
+      <Tooltip title="LinkedIn">
+        <a href="https://www.linkedin.com/in/kasey-valdez/"><i className="fa-brands fa-linkedin"></i></a>
+      </Tooltip>
+      <Tooltip title="Caution! Opens your email! I get people hate that (I'm people)">
+        <a href="mailto:kaseyvaldez98@gmail.com?subject=Hello Cool Person!"><i className="fa-solid fa-envelope"></i></a>
+      </Tooltip>
+      </div>
     </div>
   )
 }
