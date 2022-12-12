43edb028aa37cf22c48da7f1bce2d717d4afe27c
kethnal <kaseyvaldez@hotmail.com>
2022-12-12 14:32:36 -0800

    Edit header margins

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,8 +3,10 @@ export default function Projects() {
 
   return (
     <section id="projects" className="page">
-      <h1>Some stuff</h1>
-      <ProjectList />
+      <div>
+        <h1>Some stuff</h1>
+        <ProjectList />
+      </div>
     </section>
   )
 }
