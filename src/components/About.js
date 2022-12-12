11f92197968199ab125bf7f126b66a5cd4469665
kethnal <kaseyvaldez@hotmail.com>
2022-12-11 23:20:31 -0800

    Ya girl can't spell

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ export default function About() {
             ></img>
           </Tooltip>
           <div className="intro">
-            Hello! I'm based in Vancouver, BC, transitioning from a background in chemistry.<br/><br/> My love for web development leans towards the front-end and executing beautiful, clean and minimalist deisigns. I'm keen to provide users an easy and intuitive way to navigate information without being overwhelmed (that's me- I'm users).
+            Hello! I'm based in Vancouver, BC, transitioning from a background in chemistry.<br/><br/> My love for web development leans towards the front-end and executing beautiful, clean and minimalist designs. I'm keen to provide users an easy and intuitive way to navigate information without being overwhelmed (that's me- I'm users).
           </div>
         </div>
         <SkillsList />
